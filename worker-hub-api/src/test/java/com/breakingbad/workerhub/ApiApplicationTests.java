db3f44a9a8a7a0d8c120c3114f86a9d3d66af287
ones1kk <ones1k95@gmail.com>
2023-01-13 00:11:03 +0900

    Clean up : Code

--- worker-hub-api/src/test/java/com/breakingbad/workerhub/ApiApplicationTests.java
+++ worker-hub-api/src/test/java/com/breakingbad/workerhub/ApiApplicationTests.java
@@ -1,12 +1,9 @@
 package com.breakingbad.workerhub;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootApplication
 public class ApiApplicationTests {
-
 
     public void contextLoads() {
     }
