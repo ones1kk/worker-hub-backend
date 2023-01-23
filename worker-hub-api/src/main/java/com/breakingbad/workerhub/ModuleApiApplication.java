9b493d89d0171f14bbde0cb773d9d88bc90a669e
ones1kk <ones1k95@gmail.com>
2023-01-23 19:37:09 +0900

    @EnableJpaAuditing 어노테이션 추가

--- worker-hub-api/src/main/java/com/breakingbad/workerhub/ModuleApiApplication.java
+++ worker-hub-api/src/main/java/com/breakingbad/workerhub/ModuleApiApplication.java
@@ -2,8 +2,10 @@ package com.breakingbad.workerhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
