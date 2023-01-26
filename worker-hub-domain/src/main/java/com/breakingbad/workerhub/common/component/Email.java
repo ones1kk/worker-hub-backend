c47d306d5afc94b92784ac224228415b32b68b54
ones1kk <ones1k95@gmail.com>
2023-01-26 20:04:29 +0900

    Feat : @Embeddable > Email, PhoneNumber

--- worker-hub-domain/src/main/java/com/breakingbad/workerhub/common/component/Email.java
+++ worker-hub-domain/src/main/java/com/breakingbad/workerhub/common/component/Email.java
@@ -1,14 +1,35 @@
 package com.breakingbad.workerhub.common.component;
 
+import io.github.ones1kk.assertion.core.Asserts;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Transient;
+
+import java.util.regex.Pattern;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Getter
+@NoArgsConstructor(access = PROTECTED)
 @Embeddable
 public class Email {
 
     @Column(name = "email")
     private String email;
+
+    @Transient
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("[a-zA-Z\\d-_.]{2,}.+?@.{2,}?\\..{2,}$");
+
+    public Email(String email) {
+        Asserts.that(email).isNotNull().hasText().matches(EMAIL_PATTERN);
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return email;
+    }
 }
