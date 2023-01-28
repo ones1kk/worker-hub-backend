b4e82b1732e0e7177c1886938798a182eeaa9b5b
ones1kk <ones1k95@gmail.com>
2023-01-29 02:01:59 +0900

    Remove : 임시 클래스 삭제

--- worker-hub-domain/src/main/java/com/breakingbad/workerhub/repository/CustomMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.breakingbad.workerhub.repository;
-
-import com.breakingbad.workerhub.domain.CustomMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomMemberRepository extends JpaRepository<CustomMember, Long> {
-}
