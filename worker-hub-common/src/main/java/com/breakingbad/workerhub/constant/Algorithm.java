91aa7b190f7521e7a8484aa7be3cecfcb144c1ea
ones1kk <ones1k95@gmail.com>
2023-01-29 02:39:20 +0900

    알고리즘 enum 추가

---
+++ worker-hub-common/src/main/java/com/breakingbad/workerhub/constant/Algorithm.java
@@ -0,0 +1,16 @@
+package com.breakingbad.workerhub.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Algorithm {
+
+    AES_CBC_PKCS5PADDING("AES/CBC/PKCS5Padding"),
+
+    HS256("HS256");
+
+    private final String value;
+
+}
