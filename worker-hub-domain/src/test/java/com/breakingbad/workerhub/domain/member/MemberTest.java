352638d3958db39f665d77895378cf1dc015562d
ones1kk <ones1k95@gmail.com>
2023-02-07 23:44:27 +0900

    코드 정리

--- worker-hub-domain/src/test/java/com/breakingbad/workerhub/domain/member/MemberTest.java
+++ worker-hub-domain/src/test/java/com/breakingbad/workerhub/domain/member/MemberTest.java
@@ -2,19 +2,24 @@ package com.breakingbad.workerhub.domain.member;
 
 import com.breakingbad.workerhub.core.config.AuditConfig;
 import com.breakingbad.workerhub.core.crypto.Crypto;
-import com.breakingbad.workerhub.domain.component.Email;
 import com.breakingbad.workerhub.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Import(value = {AuditConfig.class})
+@Disabled
 class MemberTest {
 
     @MockBean
@@ -28,11 +33,7 @@ class MemberTest {
 
     @Test
     void test() {
-        Member member = Member.builder()
-                .email(new Email("ones1k95@gmail.com"))
-                .build();
-
-        memberRepository.save(member);
+        memberRepository.save(new Member());
 
         Member findMember = memberRepository.findById(1L).orElseThrow();
 
