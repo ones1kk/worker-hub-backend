fc11d4835c79c589dd3deb358e4148265978e860
ones1kk <ones1k95@gmail.com>
2023-02-08 22:26:21 +0900

    도메인 테스트 설정

--- worker-hub-domain/src/test/java/com/breakingbad/workerhub/domain/member/MemberTest.java
+++ worker-hub-domain/src/test/java/com/breakingbad/workerhub/domain/member/MemberTest.java
@@ -1,32 +1,22 @@
 package com.breakingbad.workerhub.domain.member;
 
-import com.breakingbad.workerhub.core.config.AuditConfig;
-import com.breakingbad.workerhub.core.crypto.Crypto;
+import com.breakingbad.workerhub.TestIntegrationConfig;
 import com.breakingbad.workerhub.domain.member.repository.MemberRepository;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * {@code @DataJpaTest} Sample
+ */
 @DataJpaTest
-@Import(value = {AuditConfig.class})
-@Disabled
+@Import(TestIntegrationConfig.class)
+@ActiveProfiles("test")
 class MemberTest {
-
-    @MockBean
-    private Crypto crypto;
-
-    @MockBean
-    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private MemberRepository memberRepository;
