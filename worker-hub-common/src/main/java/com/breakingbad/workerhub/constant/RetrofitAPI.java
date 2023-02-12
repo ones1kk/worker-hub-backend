ba35cbcd58d2a49fe806957a250ea727c22305f9
ones1kk <ones1k95@gmail.com>
2023-02-12 22:35:05 +0900

    상수 추가

---
+++ worker-hub-common/src/main/java/com/breakingbad/workerhub/constant/RetrofitAPI.java
@@ -0,0 +1,8 @@
+package com.breakingbad.workerhub.constant;
+
+public class RetrofitAPI {
+
+    public static final String KASI_API_ADDRESS = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/";
+
+}
+
