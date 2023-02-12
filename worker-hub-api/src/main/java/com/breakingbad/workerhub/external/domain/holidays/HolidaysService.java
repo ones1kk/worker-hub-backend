3773f3ab88e97218aa41063f3e65e6f2e86a2a6f
ones1kk <ones1k95@gmail.com>
2023-02-12 22:33:51 +0900

    공휴일 도메인 추가

---
+++ worker-hub-api/src/main/java/com/breakingbad/workerhub/external/domain/holidays/HolidaysService.java
@@ -0,0 +1,11 @@
+package com.breakingbad.workerhub.external.domain.holidays;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class HolidaysService {
+
+
+}
---
+++ worker-hub-domain/src/main/java/com/breakingbad/workerhub/core/converter/YearConverter.java
@@ -0,0 +1,22 @@
+package com.breakingbad.workerhub.core.converter;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Convert;
+import java.time.Year;
+
+@Convert
+@Component
+public class YearConverter implements AttributeConverter<Year, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(Year attribute) {
+        return attribute.getValue();
+    }
+
+    @Override
+    public Year convertToEntityAttribute(Integer dbData) {
+        return Year.of(dbData);
+    }
+}
