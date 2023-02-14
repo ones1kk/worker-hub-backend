7872e64141228ea85ce1119bd48818e8441fb00f
ones1kk <ones1k95@gmail.com>
2023-02-15 00:08:45 +0900

    api 모듈 테스트 config 추가

--- worker-hub-api/src/test/java/com/breakingbad/workerhub/IntegrationTestConfig.java
+++ worker-hub-api/src/test/java/com/breakingbad/workerhub/IntegrationTestConfig.java
@@ -3,14 +3,12 @@ package com.breakingbad.workerhub;
 import com.breakingbad.workerhub.core.config.AuditConfig;
 import com.breakingbad.workerhub.core.config.RetrofitConfig;
 import com.breakingbad.workerhub.core.crypto.Crypto;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Import({AuditConfig.class, RetrofitConfig.class})
-@EnableConfigurationProperties
+@Import(value = {AuditConfig.class, RetrofitConfig.class})
 public class IntegrationTestConfig {
 
     @MockBean
