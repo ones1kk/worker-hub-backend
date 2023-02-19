b51471f0d64667ac1dad62417fa7e49080e13522
ones1kk <ones1k95@gmail.com>
2023-02-19 21:30:50 +0900

    패키지 이동

--- worker-hub-api/src/main/java/com/breakingbad/workerhub/core/config/SchedulerConfig.java
+++ worker-hub-api/src/main/java/com/breakingbad/workerhub/core/config/SchedulerConfig.java
@@ -1,4 +1,4 @@
-package com.breakingbad.workerhub.core.scheduler;
+package com.breakingbad.workerhub.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
